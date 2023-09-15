f33208a05235585ec49cac9146fac38da587a8ab
Yoann Bourdex <yoann.bourdex@gmail.com>
2023-09-15 13:32:07 +0200

    start setup-board

---
+++ app/Core/setup-board/use-cases/initiate-dungeons.use-case.ts
@@ -0,0 +1,24 @@
+import { randomUUID } from 'crypto'
+import Dungeon, { DungeonEventName, DungeonName } from '../dungeon/dungeon'
+
+export interface DungeonRepository {
+  create(dungeon: Dungeon): Promise<Dungeon>
+  countAll(): Promise<number>
+}
+
+interface InitiateDungeonsUseCaseInput {
+  dungeonCount: number
+}
+
+export default class InitiateDungeonsUseCase {
+  constructor(private readonly dungeonRepository: DungeonRepository) {}
+
+  public async apply(input: InitiateDungeonsUseCaseInput) {
+    const countOfExistingDungeons = await this.dungeonRepository.countAll()
+    for (let i = input.dungeonCount - countOfExistingDungeons; i > 0; i--) {
+      await this.dungeonRepository.create(
+        new Dungeon(new DungeonName(randomUUID().toString()), [DungeonEventName.LAVA])
+      )
+    }
+  }
+}
