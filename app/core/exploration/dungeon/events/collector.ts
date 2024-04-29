538522220352121c7c4ba83f2c1b10bce0d8e9d1
Yoann Bourdex <yoann.bourdex@gmail.com>
2024-04-29 10:23:17 +0200

    improve collector event

--- app/core/exploration/dungeon/events/collector.ts
+++ app/core/exploration/dungeon/events/collector.ts
@@ -11,6 +11,7 @@ export default class Collector extends EventBase<Player> {
     const note = super.resolve(player)
     const moneyCount = player.countTag('money')
     if (moneyCount > 0) {
+      player.backpack.removeAllFromTag('money')
       note.add(new Note(`Il est intéressé par mes ${moneyCount} objets de valeurs`, moneyCount))
     } else {
       note.add(new Note("Je n'ai rien pour lui malheureusement"))
