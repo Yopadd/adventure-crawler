2f965ecfb439c27a00fec7c4b29602c0d537cad4
Yoann Bourdex <yoann.bourdex@gmail.com>
2024-05-03 16:43:19 +0200

    fix rate limiter

--- app/middleware/global_rate_limite_middleware.ts
+++ app/middleware/global_rate_limite_middleware.ts
@@ -4,15 +4,6 @@ import type { NextFn } from '@adonisjs/core/types/http'
 
 export default class GlobalRateLimiteMiddleware {
   async handle(ctx: HttpContext, next: NextFn) {
-    /**
-     * Middleware logic goes here (before the next call)
-     */
-    throttle(ctx, next)
-
-    /**
-     * Call next method in the pipeline and return its output
-     */
-    const output = await next()
-    return output
+    return throttle(ctx, next)
   }
 }
