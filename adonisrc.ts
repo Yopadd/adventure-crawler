5370bfddd7bfaedc5d2cd6612a0703b150acb43b
Yoann Bourdex <yoann.bourdex@gmail.com>
2024-06-29 16:28:36 +0200

    add cors config

--- adonisrc.ts
+++ adonisrc.ts
@@ -36,6 +36,7 @@ export default defineConfig({
     () => import('@adonisjs/lucid/database_provider'),
     () => import('@adonisjs/redis/redis_provider'),
     () => import('@adonisjs/limiter/limiter_provider'),
+    () => import('@adonisjs/cors/cors_provider'),
   ],
 
   /*
