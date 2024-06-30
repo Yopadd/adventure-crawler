c855e2f7b1e6817716a6bd82d1ff89722a7f9a84
Yoann Bourdex <yoann.bourdex@gmail.com>
2024-06-30 10:35:55 +0200

    change wording thief event

--- app/core/exploration/adventure/events/thief.ts
+++ app/core/exploration/adventure/events/thief.ts
@@ -4,13 +4,16 @@ import Note from '#app/core/exploration/player/report/note/note'
 
 export default class Thief extends EventBase<Player> {
   constructor() {
-    super('Thief', 'Au voleur !')
+    super(
+      'Thief',
+      "Je me baladais dans un marché, au milieu de la foule, quand j'ai entendu un cri dans ma direction 'Au voleur !'"
+    )
   }
 
   public resolve(player: Player, note: Note): boolean {
     super.resolve(player, note)
     if (player.hasTag('weapon')) {
-      note.add(new Note("J'avais de quoi me défendre !", 1))
+      note.add(new Note("Heureusement, j'avais de quoi me défendre ! Il est reparti bredouille", 1))
     } else {
       player.backpack.empty()
       note.add(new Note("Je n'ai rien pu faire, il m'a tout pris !", 0))
