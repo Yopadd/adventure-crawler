17cf887bd86d8884ff797060558e8b06d4ea89a6
Yoann Bourdex <yoann.bourdex@gmail.com>
2024-06-30 14:06:50 +0200

    update wording in wolfs event

--- app/core/exploration/adventure/events/wolfs.ts
+++ app/core/exploration/adventure/events/wolfs.ts
@@ -40,26 +40,26 @@ export default class Wolfs extends EventBase<Player> {
       )
       return false
     } else {
-      note.add(
-        new Note(
-          'Ils étaient trop nombreux pour moi... Peut-être que je pouvais les calmer avec un peu de nourriture',
-          player.countTag('weapon')
-        )
-      )
+      note.add(new Note('Ils étaient trop nombreux pour moi...', player.countTag('weapon')))
     }
 
     if (player.hasTag('animal')) {
       const animalCount = player.countTag('animal')
       if (animalCount > 1) {
-        note.add(new Note("Mes animaux m'ont permis faire diversion et de m'échapper", 2))
+        note.add(new Note("Mais mes animaux m'ont permis faire diversion et de m'échapper", 2))
       } else {
-        note.add(new Note("Mon animal m'a permis faire diversion et de m'échapper", 1))
+        note.add(new Note("Mais mon animal m'a permis faire diversion et de m'échapper", 1))
       }
     } else if (player.hasTag('meat')) {
       player.backpack.removeAllFromTag('meat')
-      note.add(new Note("J'avais de quoi les calmer un peu", 1))
+      note.add(
+        new Note(
+          `Mais j'avais de quoi calmer ${this.wolfsCount.get() > 1 ? 'leur faim' : 'sa faim'}`,
+          1
+        )
+      )
     } else {
-      note.add(new Note("Je n'avais absolument rien, j'ai dû me débrouiller...", -1))
+      note.add(new Note("j'ai dû me débrouiller pour fuir...", -1))
     }
     return false
   }
